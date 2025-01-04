cbb38b23759145102e9cdb7f460b44a16928939d
Henrique GC <henriq4net@gmail.com>
2025-01-04 11:31:07 -0300

    feat: create model and mock

---
+++ lib/modules/home/models/item_model.dart
@@ -0,0 +1,58 @@
+class Item {
+  String name;
+  String description;
+  bool isCompleted;
+
+  Item({
+    required this.name,
+    required this.description,
+    required this.isCompleted,
+  });
+}
+
+class ItemList {
+  List<Item> items = [
+    Item(
+      name: 'Design sign up flow',
+      description:
+          'By the time a prospect arrives at your signup page, in most cases...',
+      isCompleted: false,
+    ),
+    Item(
+      name: 'Design sign up flow',
+      description:
+          'By the time a prospect arrives at your signup page, in most cases...',
+      isCompleted: false,
+    ),
+    Item(
+      name: 'Design sign up flow',
+      description:
+          'By the time a prospect arrives at your signup page, in most cases...',
+      isCompleted: false,
+    ),
+    Item(
+      name: 'Design sign up flow',
+      description:
+          'By the time a prospect arrives at your signup page, in most cases...',
+      isCompleted: false,
+    ),
+    Item(
+      name: 'Design sign up flow',
+      description:
+          'By the time a prospect arrives at your signup page, in most cases...',
+      isCompleted: false,
+    ),
+    Item(
+      name: 'Design sign up flow',
+      description:
+          'By the time a prospect arrives at your signup page, in most cases...',
+      isCompleted: false,
+    ),
+    Item(
+      name: 'Design sign up flow',
+      description:
+          'By the time a prospect arrives at your signup page, in most cases...',
+      isCompleted: false,
+    ),
+  ];
+}
