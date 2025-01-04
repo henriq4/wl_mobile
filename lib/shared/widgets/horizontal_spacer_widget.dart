b736581bd55c10ec8012f67460842d760cbb8eb5
Henrique GC <henriq4net@gmail.com>
2025-01-04 11:31:55 -0300

    feat: shared util widget - horizontal spacer

---
+++ lib/shared/widgets/horizontal_spacer_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HorizontalSpacerWidget extends StatelessWidget {
+  const HorizontalSpacerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: 12);
+  }
+}
